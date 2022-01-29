59debc695afabf29a1c6325b2422eb1cce79c839
lmagruu <magg4ms@gmail.com>
2022-01-29 00:56:01 -0400

    create id api

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,29 +3,6 @@ import { Stack, Divider } from "@mui/material";
 import QuoteList from "../src/components/quote/QuoteList";
 import HeaderCustom from "../src/components/UI/Header";
 
-export const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "Isac El Guapos",
-    text: "Estoy hasta la puta madre de vivir en vzla",
-  },
-  {
-    id: "q2",
-    author: "Yo mismo soy",
-    text: "Me cago en Juan Pablo, segunda cita",
-  },
-  {
-    id: "q3",
-    author: "Ruben",
-    text: "A donde tan peinada, tercera cita",
-  },
-  {
-    id: "q4",
-    author: "Ruben",
-    text: "Hola sexo, cuarta cita",
-  },
-];
-
 const Home: NextPage = () => {
   return (
     <Stack spacing={2} direction={"column"} mb={2}>
