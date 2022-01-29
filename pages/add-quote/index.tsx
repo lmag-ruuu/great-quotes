91e5f247dce0a09d620d95413a4890b713831b00
lmagruu <magg4ms@gmail.com>
2022-01-29 15:54:26 -0400

    TEST AGAIN PATH API

--- pages/add-quote/index.tsx
+++ pages/add-quote/index.tsx
@@ -3,6 +3,7 @@ import { NextPage } from "next";
 
 const AddQuote: NextPage = () => {
   console.log(process.env.MONGODB_URI);
+  console.log(process.env.API_URL);
   return <Form />;
 };
 export default AddQuote;
