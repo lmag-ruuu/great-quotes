095052f4e6646d6657f32f4a89c83ab259363550
lmagruu <magg4ms@gmail.com>
2022-01-29 15:59:29 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    API_URL: "https://great-quotes-53i8jhpra-lmag-ruuu.vercel.app/api/quotes",
-    MONGODB_URI:
-      "mongodb+srv://ruben:Gf0UD4JuZwp5Wtgb@cluster0.ejlc8.mongodb.net/quotesDB?retryWrites=true&w=majority",
-  },
 };
 
 module.exports = nextConfig;
