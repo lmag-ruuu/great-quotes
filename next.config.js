61acd6bf77d4aece4b34037008e14f1b3cb1cd18
lmagruu <magg4ms@gmail.com>
2022-01-29 15:57:35 -0400

    test other path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    API_URL: "http://localhost:3000/api/quotes",
+    API_URL: "https://great-quotes-53i8jhpra-lmag-ruuu.vercel.app/api/quotes",
     MONGODB_URI:
       "mongodb+srv://ruben:Gf0UD4JuZwp5Wtgb@cluster0.ejlc8.mongodb.net/quotesDB?retryWrites=true&w=majority",
   },
