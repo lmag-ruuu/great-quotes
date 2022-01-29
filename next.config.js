36b86373c3055197fde68501902635acdb93b374
lmagruu <magg4ms@gmail.com>
2022-01-29 01:49:39 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    API_URL: process.env.API_URL,
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
