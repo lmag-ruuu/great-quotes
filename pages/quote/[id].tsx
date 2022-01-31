0c665fad53f096b9de21c744a8689f30da62711c
lmagruu <magg4ms@gmail.com>
2022-01-30 20:02:56 -0400

    process env on vercel

--- pages/quote/[id].tsx
+++ pages/quote/[id].tsx
@@ -110,8 +110,8 @@ function QuoteDetail(props: QuoteDetailProp) {
 export async function getServerSideProps(context: any) {
   const quoteId = context.query.id;
   // get todo data from API
-  const url =
-    "mongodb+srv://ruben:Gf0UD4JuZwp5Wtgb@cluster0.ejlc8.mongodb.net/quotesDB?retryWrites=true&w=majority";
+
+  const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : "";
   const client = new MongoClient(url);
   const quotes: Array<quote> = [];
 
