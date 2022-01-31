06ae1bfe6a211efb82c232693c8a37c87f4a90fe
lmagruu <magg4ms@gmail.com>
2022-01-30 20:05:10 -0400

    delete unused interface

--- utils/types.ts
+++ utils/types.ts
@@ -1,10 +1,3 @@
-export interface ResponseFuncs {
-  GET?: Function;
-  POST?: Function;
-  PUT?: Function;
-  DELETE?: Function;
-}
-
 // Interface to define our Todo model on the frontend
 export interface quote {
   _id?: string;
